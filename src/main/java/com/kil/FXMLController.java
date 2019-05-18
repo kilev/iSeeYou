d3e9cbe652f44593d830d4edad405eebd363db30
kooler.kilevpc@gmail.com <kooler.kilevpc@gmail.com>
2019-05-18 14:15:03 +0700

    initial commit

---
+++ src/main/java/com/kil/FXMLController.java
@@ -0,0 +1,19 @@
+package com.kil;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class FXMLController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
